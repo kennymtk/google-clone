339a4f8dd5e44e3359aa0f58bd25b3368f10feab
kennymtk <45045317+kennymtk@users.noreply.github.com>
2023-03-04 01:14:27 +0800

    tailwind

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,12 @@
 import "./globals.css";
+// import { Favicon } from "./components/svgs";
 
 export const metadata = {
   title: "Google Clone",
   description: "Google Clone with Next.js",
+  // icons: {
+  //   icon: Favicon,
+  // },
 };
 
 export default function RootLayout({ children }) {
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import React from "react";
 
-export default function page() {
-  return <div>page</div>;
+export default function Home() {
+  return <div className="text-red-600 text-3xl">Home</div>;
 }
