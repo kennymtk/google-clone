5b896c9df0d2c19d9a2b7625388d233e9570e3de
kennymtk <45045317+kennymtk@users.noreply.github.com>
2023-03-16 22:41:13 +0800

    image result

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,5 +1,35 @@
-import React from "react";
+import Link from "next/link";
+import WebSearchResult from "@/components/WebSearchResult";
 
-export default function WebSearchPage() {
-  return <div>WebSearchPage</div>;
+export default async function WebSearchPage({ searchParams }) {
+  await new Promise((resolve) => setTimeout(resolve, 10000));
+
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
+  );
+
+  if (!response.ok) {
+    throw new Error("Something went wrong");
+  }
+
+  const data = await response.json();
+  // console.log(data);
+
+  const results = data.items;
+
+  if (!results) {
+    return (
+      <div className="flex flex-col justify-center items-center pt-10">
+        <h1 className="text-3xl mb-4">No results found</h1>
+        <p className="text-lg">
+          Try searching for something else or go back to the homepage{" "}
+          <Link href="/" className="text-blue-500">
+            Home
+          </Link>
+        </p>
+      </div>
+    );
+  }
+
+  return <>{results && <WebSearchResult results={data} />}</>;
 }
---
+++ src/components/WebSearchResult.jsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import Parser from "html-react-parser";
+
+export default function WebSearchResult({ results }) {
+  return (
+    <div className="w-full mx-auto px-3 pb-24 sm:pl-6 lg:pl-52">
+      <p className="text-gray-500 text-sm mb-5 mt-3">
+        About {results.searchInformation?.formattedTotalResults} results (
+        {results.searchInformation?.formattedSearchTime} seconds)
+      </p>
+      {results.items?.map((result) => (
+        <div key={result.link} className="mb-8 max-w-xl">
+          <div className="group flex flex-col">
+            <Link className="text-sm truncate" href={result.link}>
+              {result.formattedUrl}
+            </Link>
+            <Link
+              className="group-hover:underline decoration-blue-800 text-xl truncate font-medium text-blue-800"
+              href={result.link}
+            >
+              {result.title}
+            </Link>
+          </div>
+          <p className="">{Parser(result.htmlSnippet)}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
